0042aa552ad725ecd64e06f30c194b673d899605
Calvin Leung <leung@wantedly.com>
2024-03-11 21:11:38 +0900

    update config

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,7 +3,9 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.ruby" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
